719fb1945f1f620c49b01a8fa88d2a4d99b842b7
CSua33 <108198515+CSua33@users.noreply.github.com>
2023-02-01 16:39:35 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:iotedu.database.windows.net,1433; Database = pulsoCardiacodb", "CloudSA1ce0f26e", "{Cafu2025.}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "CloudSA1ce0f26e", "pwd" => "{Cafu2025.}", "Database" => "pulsoCardiacodb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:iotedu.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
