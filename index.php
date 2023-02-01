ea49b9e9a1ec3daa9c73598427f4156d8aea1983
CSua33 <108198515+CSua33@users.noreply.github.com>
2023-02-01 17:07:04 -0600

    Update index.php

--- index.php
+++ index.php
@@ -14,9 +14,21 @@ $connectionInfo = array("UID" => "CloudSA1ce0f26e", "pwd" => "{Cafu2025.}", "Dat
 $serverName = "tcp:iotedu.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 echo "Hola mundo";
-if ($conn === true) {
      echo "Connection was established";
      echo "<br>";
 
-            }
+    $tsql = "SELECT TOP 1 frecuencia FROM [dbo].[raspberry]";
+    $stmt = sqlsrv_query($conn, $tsql);
+    if ($stmt === false) {
+        echo "Error in query execution";
+        echo "<br>";
+        die(print_r(sqlsrv_errors(), true));
+    }
+    while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+        echo $row['frecuencia'] . "<br/>" ;
+    }
+    sqlsrv_free_stmt($stmt);
+    sqlsrv_close( $conn);
+                
+            
 ?>
